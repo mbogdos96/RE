5884c086cfe6e2790c2b2ec02e966f20d639b8a7
Michael Bogdos <94126459+mbogdos96@users.noreply.github.com>
2024-10-28 13:26:09 +0100

    Update SW_analysis.R

--- Experimental_data_with_code/SW_analysis.R
+++ Experimental_data_with_code/SW_analysis.R
@@ -1,7 +1,8 @@
 library(dplyr)
+library(stringr)
 
-# Load the data that is in the file which extract the value for E1/2 Fc 
-# to nomralise data and also for subfolder path
+# Load the data that is in the file which extract 
+# the value for E1/2 Fc to nomralise data 
 source("Fc_standard.R")
 
 #========================
@@ -13,18 +14,20 @@ source("Fc_standard.R")
 #==========
 # Functions
 #==========
-
 # Find the files with the square wave data
-SW_file_list <- list.files(subfolder, 
+SW_file_list <- list.files(file.path("./MBPdG10"), 
                            pattern = "SW_.*\\.csv", 
                            full.names = TRUE)
 
-# A fucntion which loops through a file list and combines & manipulates all the SW data
-# the argument just requires a list of files with the file name structured as 
+# A fucntion which loops through a file 
+# list and combines & manipulates all the SW data
+# the argument just requires a list 
+# of files with the file name structured as 
 # SW_LIGAND.csv to function
 SW_comb <- function(file_list){
   
-  # Create empty dataframe which will be filled with the rest
+  # Create empty dataframe which will 
+  # be filled with the rest
   SWs_all <- data.frame()
   
   for (file_name in file_list) {
@@ -34,12 +37,14 @@ SW_comb <- function(file_list){
                  "\\1",
                  basename(file_name))
   
-  # Create a temporary dataframe to load in and manipulate data
+  # Create a temporary dataframe to 
+  # load in and manipulate data
   SW_temp_df <- read.csv(file_name,
                          check.names = F) 
   
   # Check if column names need adjustment
-  if (!all(c("Potential", "Current") %in% colnames(SW_temp_df))) {
+  if (!all(c("Potential", 
+             "Current") %in% colnames(SW_temp_df))) {
     
    # Adjust column names
     colnames(SW_temp_df) <- c("Potential", 
@@ -55,10 +60,11 @@ SW_comb <- function(file_list){
   
   # Adjust potential and add ligand name column
   SW_temp_df <- SW_temp_df %>%
-    mutate(Potential = (Potential - Fc_E_avg)) %>% # Adjust potential to be vs Fc E1/2
-    mutate(Ligand = L_name) # Add a column which gives the ligand name
+    mutate(Potential = (Potential - Fc_E_avg)) %>%
+    mutate(Ligand = L_name)
   
-  # Append the temporary modified data to a single dataframe with all ligand data
+  # Append the temporary modified data to a 
+  # single dataframe with all ligand data
   SWs_all <- rbind(SWs_all,
                    SW_temp_df) 
   
@@ -66,41 +72,52 @@ SW_comb <- function(file_list){
   
   # Final function output
   return(SWs_all)
-} # End of function
+}
 
-# Make a function which picks peaks out of a combined dataframe of Square Wave data
+# Make a function which picks peaks out of a 
+# combined dataframe of Square Wave data
 # The arguments that the function needs are: 
 # df = dataframe (usually outputted by SW_comb)
 # V = voltage window in which you want to look for peaks 
 # I = current threshold for looking for peaks,
-# no_peaks = the number of peaks you want to detect, 1 will give you the first peak found
-# starting from negative potentials and going to more positive. The variable I should not be set 
-# too low as the baseline is small but technically sinusoidal so it will give peaks detected where
+# no_peaks = the number of peaks you want to detect, 
+# 1 will give you the first peak found
+# starting from negative potentials and 
+# going to more positive. The variable I should not be set 
+# too low as the baseline is small 
+# but technically sinusoidal so it will 
+# give peaks detected where
 # there are none really.
-# Tldr; SW_peaks(data,Voltage_threshold,Current_threshold,number of peaks to be detected)
+# Tldr; SW_peaks(data,
+# Voltage_threshold,
+# Current_threshold,number of peaks to be detected)
 SW_peaks <- function(df,V,I,no_peaks){
   
   # Append data to peak potential data frame
   peak_potential_df <- df %>%
     filter(Potential >= V & Current >= I) %>%
-    filter(row_number() %in% (which(diff(sign(diff(Current))) < 0) + 1)) %>% # Filter only for rows where peaks are found
-    arrange(Potential) %>% # Arrange the df in ascending order of Potential
-    group_by(Ligand) %>% # Group by Ligand
-    slice_head(n = no_peaks) %>% # Keep only the first row for every ligand
-    ungroup() %>% # This is needed for later plotting purposes
-    arrange(Potential) # Also for plotting
+    filter(row_number() %in% (which(
+      diff(sign(diff(Current))) < 0) + 1)) %>%
+    arrange(Potential) %>%
+    group_by(Ligand) %>%
+    slice_head(n = no_peaks) %>%
+    ungroup() %>%
+    arrange(Potential)
   
   # Final output of the function
   return(peak_potential_df)
   
-} # End of function
+}
 
 #=========
 # Datasets
 #=========
 # Square wave data
-# All ligands - add a column that classifies the ligand
+# All palladacycles - add a column that classifies 
+# the coord no
 all_SW <- SW_comb(SW_file_list) %>%
+  filter(!(str_detect(Ligand,
+                    "_free"))) %>%
   mutate(L_type = ifelse(
     Ligand %in% c("BIPHEP", 
                 "dcype", 
@@ -109,6 +126,11 @@ all_SW <- SW_comb(SW_file_list) %>%
                 "dppm"),
     "bidentate",
     "monodentate"))
+
+# All free ligands
+all_free_L_SW <- SW_comb(SW_file_list) %>%
+  filter(str_detect(Ligand,
+                    "_free"))
 
 # Extracted peaks data
 # Get the first oxidation potential 
@@ -120,19 +142,28 @@ first_oxidation_potential_all <- SW_peaks(all_SW,
 
 # Bidentate
 first_oxidation_potential_bidentate <- SW_peaks(all_SW %>%
-                                                  filter(L_type == "bidentate"),
+                                                  filter(
+                                        L_type == "bidentate"),
                                                 0.5,
                                                 1e-6,
                                                 1)
 
 # Monodentate
 first_oxidation_potential_monodentate <- SW_peaks(all_SW %>%
-                                                    filter(L_type == "monodentate"),
+                                                    filter(
+                                      L_type == "monodentate"),
                                                   0.5,
                                                   1e-6,
                                                   1)
 
-# Get the first 4 peaks for all the data - this should find them, there should be 2 or 3
+# All free ligands
+first_ox_free_L <- SW_peaks(all_free_L_SW,
+                            0.3,
+                            1e-6,
+                            1)
+
+# Get the first 4 peaks for all the data - this 
+# should find them, there should be 2 or 3
 # All ligands
 first_three_oxidation_potentials_all <- SW_peaks(all_SW,
                          0.5,
@@ -140,15 +171,19 @@ first_three_oxidation_potentials_all <- SW_peaks(all_SW,
                          4)
 
 # Bidentate
-first_three_oxidation_potentials_bidentate <- SW_peaks(all_SW %>%
-                                                  filter(L_type == "bidentate"),
+first_three_oxidation_potentials_bidentate <- SW_peaks(
+                                                all_SW %>%
+                                                  filter(
+                                      L_type == "bidentate"),
                                                 0.5,
                                                 1e-6,
                                                 4)
 
 # Monodentate
-first_three_oxidation_potentials_monodentate <- SW_peaks(all_SW %>%
-                                                    filter(L_type == "monodentate"),
+first_three_oxidation_potentials_monodentate <- SW_peaks(
+                                                  all_SW %>%
+                                                    filter(
+                                    L_type == "monodentate"),
                                                   0.5,
                                                   1e-6,
                                                   4)
